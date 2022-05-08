f3082c1c6c3e03f7316ad0975c112367a236b7e3
Yarra9 <105175841+Yarra9@users.noreply.github.com>
2022-05-09 00:33:41 +0300

    Add files via upload

---
+++ A.py
@@ -0,0 +1,9 @@
+#question1 A
+students=['yara','zubaida','laial','ali','sara']
+name=input('input a student name:')
+if name in students:
+    print("student is graduated")
+else:
+    print("student is not graduated")
+
+
